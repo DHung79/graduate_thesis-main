5f1c9a54f6c4413572b1be98fce9e93bb89a4b4e
daoleduyhung <daoleduyhung04@gmail.com>
2022-06-18 12:45:10 +0700

    change name

--- lib/routes/route_delegate.dart
+++ lib/routes/route_delegate.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import '/routes/route_names.dart';
-import '../screens/generate_password/generate_password_homepage.dart';
+import '../screens/generate_password_screen/generate_password_screen.dart';
 import '../screens/home_screen/home_screen.dart';
 import '../screens/not_found/page_not_found_screen.dart';
 import '../screens/onboarding/splash_screen.dart';
 import '../screens/onboarding/wellcome_screen.dart';
 import '../screens/sign_in_screen/sign_in_screen.dart';
 import '../screens/sign_up_screen/sign_up_screen.dart';
+
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings path) {
     String routePath = path.name ?? '';
